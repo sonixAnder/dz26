8246da46241a34c256e08f8b3a7455db6f951650
sonixAnder <148888208+sonixAnder@users.noreply.github.com>
2024-04-13 12:35:56 +0300

    Add files via upload

---
+++ number of characters/number of characters/number of characters.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    setlocale(LC_ALL, "ru");
+    string str;
+    char ch;
+    int count = 0;
+
+    cout << "Введите строку символов: ";
+    getline(cin, str);
+
+    cout << "Введите искомый символ: ";
+    cin >> ch;
+
+    for (char c : str)
+    {
+        if (c == ch) 
+        {
+            count++;
+        }
+    }
+
+    cout << "Символ '" << ch << "' встречается в строке " << count << " раз." << endl;
+
+    return 0;
+}
---
+++ the number of all characters/the number of all characters/the number of all characters.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    string inputString;
+
+    // Запрос у пользователя ввода строки
+    cout << "Введите строку: ";
+    getline(cin, inputString);
+
+    int letterCount = 0;
+    int digitCount = 0;
+    int otherCount = 0;
+
+    // Подсчет количества букв, цифр и остальных символов в строке
+    for (char c : inputString)
+    {
+        if (isalpha(c))
+        {
+            letterCount++;
+        }
+        else if (isdigit(c))
+        {
+            digitCount++;
+        }
+        else
+        {
+            otherCount++;
+        }
+    }
+
+    // Вывод результатов
+    cout << "Количество букв: " << letterCount << endl;
+    cout << "Количество цифр: " << digitCount << endl;
+    cout << "Количество других символов: " << otherCount << endl;
+
+    return 0;
+}
